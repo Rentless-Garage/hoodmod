2b7efbd23ec0ec9e0a7aa695501e48d6c6173604
SynicalMX <70176240+SynicalMX@users.noreply.github.com>
2024-06-08 19:54:33 -0400

    first commit

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+def main():
+	# print takes a string argument for outputting text to the console
+	print("Hello, world!")
+
+# this makes sure the code is being ran in the context we want
+# this is mostly edge-case bullshit but its common knowledge
+if __name__ == "__main__":
+	main()
