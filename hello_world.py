6e005872c61910914a9f176c89de41e6e0952031
LONEST0NER <142952536+LONEST0NER@users.noreply.github.com>
2024-06-08 20:20:56 -0400

    pygame

--- hello_world.py
+++
@@ -1,8 +0,0 @@
-def main():
-	# print takes a string argument for outputting text to the console
-	print("Hello, world!")
-
-# this makes sure the code is being ran in the context we want
-# this is mostly edge-case bullshit but its common knowledge
-if __name__ == "__main__":
-	main()
